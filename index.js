54fc1bc7cb86c97e63c271cb9b8be699fcaa422e
Santiago Paredes <santiagoparedes97@gmail.com>
2019-09-12 09:51:10 -0300

    sigo probando a ver si me importa correctamente las cosas en el creador

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import pbBlocks from "./pb-blocks";
 import pbBlockBuilder from "./pbBlockBuilder";
 import pbToolboxBuilder from "./pbToolboxBuilder";
 
-export default {pbBlocks,pbBlockBuilder,pbToolboxBuilder}
+export {pbBlocks,pbBlockBuilder,pbToolboxBuilder}
