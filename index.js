042184e4ff028d764a4c5fccd1e6027f543aaa21
Santiago Paredes <santiagoparedes97@gmail.com>
2019-09-12 09:31:15 -0300

    0.0.6

---
+++ index.js
@@ -0,0 +1,5 @@
+import pbBlocks from "./pb-blocks";
+import pbBlockBuilder from "./pbBlockBuilder";
+import pbToolboxBuilder from "./pbToolboxBuilder";
+
+export default {pbBlocks,pbBlockBuilder,pbToolboxBuilder}
